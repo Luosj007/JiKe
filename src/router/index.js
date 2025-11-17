bb3a718e50e3259efbe5b4a1d9188fbcc97021b8
Lsjljj <14945919+lsjljj@user.noreply.gitee.com>
2025-11-17 21:46:16 +0800

    Layout结构

--- src/router/index.js
+++ src/router/index.js
@@ -3,7 +3,7 @@
 import Layout from '@/pages/Layout'
 import Login from '@/pages/Login'
 
-import {createBrowserRouter } from 'react-router-dom'
+import { createBrowserRouter } from 'react-router-dom'
 import { AuthRoute } from '@/components/AuthRoute'
 
 // 配置路由
