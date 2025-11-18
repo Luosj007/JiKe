540b98ded55b0059812b77c2832e860fa803a031
Lsjljj <14945919+lsjljj@user.noreply.gitee.com>
2025-11-18 11:32:20 +0800

    echarts组件封装

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,4 +1,20 @@
+import { BarChart } from './BarChart'
+
 const Home = () => {
-  return <div>Home</div>
+  return (
+    <div>
+      <BarChart
+        title={['三大框架满意度']}
+        xData={['Vue', 'React', 'Angular']}
+        sData={[2000, 5000, 1000]} />
+
+      <BarChart
+        title={['三大框架使用度']}
+        xData={['Vue', 'React', 'Angular']}
+        sData={[200, 500, 100]}
+        style={{ width: '500px', height: '400px' }} />
+    </div >
+  )
 }
+
 export default Home
