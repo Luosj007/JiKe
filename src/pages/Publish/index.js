ee1580b0b01ff495264cd2977d6708704ef9efb2
Lsjljj <14945919+lsjljj@user.noreply.gitee.com>
2025-11-18 21:39:02 +0800

    收集表单数据

--- src/pages/Publish/index.js
+++ src/pages/Publish/index.js
@@ -15,7 +15,7 @@ import './index.scss'
 import ReactQuill from 'react-quill'
 import 'react-quill/dist/quill.snow.css'
 import { useEffect, useState } from 'react'
-import { getChannelAPI } from '@/apis/article'
+import { getChannelAPI, createArticleAPI } from '@/apis/article'
 
 const { Option } = Select
 
@@ -29,6 +29,24 @@ const Publish = () => {
       }
       getChannelList()
   }, [])
+
+  // 提交表单
+  const onFinish = (formValue) => {
+    console.log(formValue)
+    const { title, content, channel_id  } = formValue
+    // 1.根据接口文档格式处理收集到的表单数据
+    const reqData = {
+      title,
+      content,
+      cover: {
+        type: 0,
+        images: []
+      },
+      channel_id
+    }
+    // 2.调用接口提交
+    createArticleAPI(reqData)
+  }
   return (
     <div className="publish">
       <Card
@@ -44,6 +62,7 @@ const Publish = () => {
           labelCol={{ span: 4 }}
           wrapperCol={{ span: 16 }}
           initialValues={{ type: 1 }}
+          onFinish={onFinish}
         >
           <Form.Item
             label="标题"
