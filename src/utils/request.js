b20e4a190030551913da818cf3a78e6442a7bf9a
Lsjljj <14945919+lsjljj@user.noreply.gitee.com>
2025-11-18 11:14:09 +0800

    处理token失效

--- src/utils/request.js
+++ src/utils/request.js
@@ -1,6 +1,7 @@
 // axios的封装
 import axios from 'axios'
-import { getToken } from '@/utils'
+import { getToken, removeToken } from '@/utils'
+import router from '@/router'
 // 1.根域名配置
 // 2.超时时间
 // 3.请求拦截器/响应拦截器
@@ -32,6 +33,12 @@ request.interceptors.response.use((response)=> {
   }, (error)=> {
     // 超出 2xx 范围的状态码都会触发该函数。
     // 对响应错误做点什么
+    // 监控401 token失效
+    if (error.response.status === 401) {
+      removeToken()
+      router.navigate('/login')
+      window.location.reload()
+    }
     return Promise.reject(error)
 })
 
